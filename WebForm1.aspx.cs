e09d85ce646cebac1b0d6fa16afdc9cfa64d77c3
zulqarnain-99 <zulqi99@gmail.com>
2020-12-13 23:43:10 +0500

    create project

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace copy_data_from_one_table_to_another_ASP.Net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
